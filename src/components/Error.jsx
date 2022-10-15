70cf87f81bf80e83e585c95c7b0b5ca7a3240c9e
Osvaldohb <osvaldonuevo@gmail.com>
2022-10-15 12:42:04 -0500

    Primer Commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react"
+
+function Error({mensaje}) {
+  return (
+    <div className="bg-red-800 text-white text-center p-3 uppercase font-bold-mb-3 rounded-md">
+    <p className="">{mensaje}</p>
+  </div>
+  )
+}
+
+export default Error
